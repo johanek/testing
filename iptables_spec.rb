9051588ad26b6a44471005eb826dd6fd83d51114
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2011-11-28 12:37:30 +0000

    wip

--- iptables_spec.rb
+++ iptables_spec.rb
@@ -4,6 +4,13 @@ require 'iptables'
 
 describe 'Iptables' do
   
+  describe '.status' do
+    it "return a number" do
+      num = Iptables.status
+      num.is_a.should == "number"
+    end
+  end
+  
   describe '.add' do
     it "create a new rule" do
       num = Iptables.status
