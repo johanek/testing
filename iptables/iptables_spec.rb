ff0908ce0233ff9d64523e514da44b6b41e6853d
johanek <johan.vandendorpe@gmail.com>
2011-12-06 15:09:02 +0000

    Update iptables/iptables_spec.rb

--- iptables/iptables_spec.rb
+++ iptables/iptables_spec.rb
@@ -14,7 +14,7 @@ describe 'Firewall' do
   end
   
   describe '.add' do
-    it "create a new rule" do
+    it "create a new host rule" do
       a = Firewall.new
       b = a.status
       a.add(:host => "google.com")
@@ -22,8 +22,17 @@ describe 'Firewall' do
       c.should > b
       a.flush
     end
+
+    it "create a new port rule" do
+      a = Firewall.new
+      b = a.status
+      a.add(:port => "389")
+      c = a.status
+      c.should > b
+      a.flush
+    end
   end
-  
+
   describe '.flush' do
     it "removes all rules" do
       a = Firewall.new
